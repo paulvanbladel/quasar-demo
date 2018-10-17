d4b0994fe7f61c48a14ad717faf66b1c08b76e3a
paul van bladel <paul.vanbladel@gmail.com>
2018-10-17 19:20:45 +0200

    initial

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import routes from './routes'
+import store from '../store'
+Vue.use(VueRouter)
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation
+ */
+
+export default function (/* { store, ssrContext } */) {
+  const Router = new VueRouter({
+    scrollBehavior: () => ({ y: 0 }),
+    routes,
+
+    // Leave these as is and change from quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    mode: process.env.VUE_ROUTER_MODE,
+    base: process.env.VUE_ROUTER_BASE
+  })
+  Router.beforeEach((to, from, next) => {
+    console.log('before each')
+    console.log(store.state.example.test)
+    next()
+  })
+  return Router
+}
